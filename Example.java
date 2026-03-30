733f81ebf17490d7f1db3fbb2a37357736eaabc4
AnjaliMadasu-18 <anjalimadasu823@gmail.com>
2026-03-30 08:39:22 +0530

    my first commit

---
+++ Example.java
@@ -0,0 +1,5 @@
+void main (){
+System.out.println(" java is simple");
+System.out.println("java is High level language");
+System.out.println("java is evergreen");
+}
---
+++ Test.java
@@ -0,0 +1,13 @@
+class Test{
+  public static void main(String[] args){
+	 int a=10, b=20;
+	 int Add=a+b;
+	 int sub=a-b;
+	 int mul=a*b;
+	 int div=a%b;
+	System.out.println(" the addition of a,b is:" +Add);
+	System.out.println(" the subtraction of two numbers is:"+sub);
+	System.out.println(" the multiplication of two numbers is:"+mul);
+	System.out.println(" the division of two numbers is:" +div);
+	}
+	}
